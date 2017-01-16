5f42a0efee690aaa85b5aaecd405063367920fcb
kathees <kathees@wso2.com>
2017-01-16 12:56:55 +0530

    Fixed for removing jsessionid from url

--- component/src/main/java/org/wso2/carbon/identity/authenticator/x509Certificate/X509CertificateAuthenticator.java
+++ component/src/main/java/org/wso2/carbon/identity/authenticator/x509Certificate/X509CertificateAuthenticator.java
@@ -75,8 +75,7 @@ public class X509CertificateAuthenticator extends AbstractApplicationAuthenticat
             String queryParams = FrameworkUtils.getQueryStringWithFrameworkContextId(
                     authenticationContext.getQueryParams(), authenticationContext.getCallerSessionKey(),
                     authenticationContext.getContextIdentifier());
-            String encodedUrl = httpServletResponse.encodeRedirectURL(authEndpoint
-                    + ("?" + queryParams));
+            String encodedUrl = (authEndpoint + ("?" + queryParams));
             httpServletResponse.sendRedirect(encodedUrl);
             if (log.isDebugEnabled()) {
                 log.debug("Request send to " + authEndpoint);
