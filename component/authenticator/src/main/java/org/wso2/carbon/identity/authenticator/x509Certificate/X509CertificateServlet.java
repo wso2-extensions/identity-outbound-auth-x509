6cda267e13b06dd7077817c3e2c8ce50e062d391
Gianluca Fares <gfares@minsait.com>
2020-02-05 23:28:20 +0100

    Add certificate to cache

--- component/authenticator/src/main/java/org/wso2/carbon/identity/authenticator/x509Certificate/X509CertificateServlet.java
+++ component/authenticator/src/main/java/org/wso2/carbon/identity/authenticator/x509Certificate/X509CertificateServlet.java
@@ -18,22 +18,30 @@
  */
 package org.wso2.carbon.identity.authenticator.x509Certificate;
 
-import org.wso2.carbon.identity.core.util.IdentityUtil;
+import java.io.IOException;
+import java.net.URLEncoder;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.net.URLEncoder;
+
+import org.apache.commons.lang.StringUtils;
+import org.wso2.carbon.identity.application.authentication.framework.config.builder.FileBasedConfigurationBuilder;
+import org.wso2.carbon.identity.application.authentication.framework.config.model.AuthenticatorConfig;
+import org.wso2.carbon.identity.application.common.cache.BaseCache;
+import org.wso2.carbon.identity.core.util.IdentityUtil;
 
 /**
  * X509 Certificate Servlet.
  */
 public class X509CertificateServlet extends HttpServlet {
 
-    private static final long serialVersionUID = -7182121722709941646L;
+	private static final long serialVersionUID = -7182121722709941646L;
 
+	private String certificateHeaderName;
+	
     /**
      * Servlet doGet.
      *
@@ -66,10 +74,33 @@ public class X509CertificateServlet extends HttpServlet {
             throw new IllegalArgumentException(X509CertificateConstants.SESSION_DATA_KEY
                     + " parameter is null.");
         } else {
+        	if(StringUtils.isNotBlank(certificateHeaderName) &&  StringUtils.isNotBlank(servletRequest.getHeader(certificateHeaderName))) {
+        		BaseCache<String, String> cert = new BaseCache<>(X509CertificateConstants.X509_CERTIFICATE_CACHE_NAME);
+	        	cert.addToCache(param, servletRequest.getHeader(certificateHeaderName));
+        	}
             commonAuthURL += "?" + X509CertificateConstants.SESSION_DATA_KEY + "="
                     + URLEncoder.encode(param, X509CertificateConstants.UTF_8) + "&"
                     + X509CertificateConstants.SUCCESS + "=true";
             servletResponse.sendRedirect(commonAuthURL);
         }
+    }
+
+    @Override
+	public void init() throws ServletException {
+    	loadSettings();
+		super.init();
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		loadSettings();
+		super.init(config);
+	}
+
+    private void loadSettings() {
+    	AuthenticatorConfig authConfig = FileBasedConfigurationBuilder.getInstance().getAuthenticatorBean(X509CertificateConstants.AUTHENTICATOR_NAME);
+    	if(authConfig != null) { 
+    		certificateHeaderName = authConfig.getParameterMap().get(X509CertificateConstants.X509_CERTIFICATE_HEADER_NAME);
+    	}
     }
 }
